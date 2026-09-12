9c8b893c567919e9af36831d35c69eadb60db656
Mike Smart <mikesmart@Mikes-MacBook-Pro-5.local>
2026-09-11 21:15:28 -0700

    Tasks added

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+tasks = [
+    {"id": 1, "title": "Study for midterm"},
+    {"id": 2, "title": "Finish assignment"},
+]
 
 @app.get("/")
 def home():
     return {"message": "Teamboard API is running"}
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
