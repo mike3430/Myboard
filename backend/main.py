3a92d75097d645103d1c2d92d9d75910ac06e9be
Mike Smart <mikesmart@Mikes-MacBook-Pro-5.local>
2026-09-13 12:43:57 -0700

    Add task delete functionality

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,6 +10,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+#when someone sends a tasks it will have a title:
+class Task(BaseModel):
+    title: str
 
 tasks = [
     {"id": 1, "title": "Study for midterm"},
@@ -22,3 +26,21 @@ def home():
 @app.get("/tasks")
 def get_tasks():
     return tasks
+
+@app.post("/tasks")
+def add_task(task: Task):
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": task.title
+    }
+    tasks.append(new_task)
+    return new_task
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return {"message": "Task deleted"}
+
+    return {"message": "Task not found"}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
   useEffect(() => {
     fetch("http://127.0.0.1:8000/tasks")
@@ -9,12 +10,54 @@ function App() {
       .then(data => setTasks(data));
   }, []);
 
+function addTask() {
+  fetch("http://127.0.0.1:8000/tasks", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      title: newTask
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      setTasks([...tasks, data]);
+      setNewTask("");
+    });
+}
+
+function deleteTask(id) {
+  fetch(`http://127.0.0.1:8000/tasks/${id}`, {
+    method: "DELETE"
+  })
+    .then(() => {
+      setTasks(tasks.filter(task => task.id !== id));
+    });
+}
+
   return (
     <div>
       <h1>Teamboard</h1>
 
+      <input
+        type="text"
+        value={newTask}
+        onChange={(event) => setNewTask(event.target.value)}
+        placeholder="Enter a task"
+      />
+
+      <button onClick={addTask}>
+        Add Task
+      </button>
+
       {tasks.map(task => (
-        <p key={task.id}>{task.title}</p>
+        <div key={task.id}>
+          <span>{task.title}</span>
+          <button onClick={() => deleteTask(task.id)}>
+            Delete
+          </button>
+        </div>
       ))}
     </div>
   );
